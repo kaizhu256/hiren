d0eaeeab82758c79afa03419a7ae92cecdfe3e54
kaizhu <kaizhu256@gmail.com>
2019-04-18 22:42:15 -0500

    - move CD/HBCD to ./

--- Make ISO.bat
+++ Make ISO.bat
@@ -1,2 +1,1 @@
-"mkisofs.exe" -R -D -J -joliet-long -o MyHBCD.iso -b HBCD/grldr -c HBCD/boot.cat -hide-joliet HBCD/boot.cat -hide HBCD/boot.cat -no-emul-boot -N -boot-info-table -V HirensBootCD  -boot-load-size 4 CD
-pause
+"mkisofs.exe" -R -D -J -joliet-long -o ../MyHBCD.iso -b HBCD/grldr -c HBCD/boot.cat -hide-joliet HBCD/boot.cat -hide HBCD/boot.cat -no-emul-boot -N -boot-info-table -V HirensBootCD  -boot-load-size 4 .
