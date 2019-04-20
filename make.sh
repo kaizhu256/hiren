6e455c3e04e28750acfd11c72b31e710f6292168
kaizhu <kaizhu256@gmail.com>
2019-04-19 20:57:10 -0500

    grldr

--- make.sh
+++ make.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 shMake () {(set -e
-    utility2 shMkisofs winxp.mini
-    cd tmp
+    utility2 shMkisofs minixp
+    rm -fr tmp/build
+    mkdir -f tmp/build
+    mv tmp/minixp.iso tmp/build
+    cp grldr tmp/build
+    cp menu.lst tmp/build
+    cd tmp/build
+    utility2 shMkisofs winxp_bootcd grldr
 )}
 
 shMake
