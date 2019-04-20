1163bb5341ae329363125e389fff4ba45616df5b
kaizhu <kaizhu256@gmail.com>
2019-04-19 21:00:50 -0500

    setupldr.bin

--- make.sh
+++ make.sh
@@ -2,12 +2,17 @@
 shMake () {(set -e
     utility2 shMkisofs minixp
     rm -fr tmp/build
-    mkdir -f tmp/build
+    mkdir -p tmp/build
     mv tmp/minixp.iso tmp/build
     cp grldr tmp/build
     cp menu.lst tmp/build
+    cp setupldr.bin tmp/build
+    (
     cd tmp/build
     utility2 shMkisofs winxp_bootcd grldr
+    )
+    mv tmp/build/tmp/winxp_bootcd.iso tmp
+    rm -fr tmp/build
 )}
 
 shMake
